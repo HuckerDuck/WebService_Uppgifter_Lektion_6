f45528a4549aa9cad3c155bd5832fe3c8df782ff
HuckerDuck aka Fredrik <fredrikmenotbrauer@gmail.com>
2025-09-01 12:19:37 +0200

    Third Commit 6 assingment should be okey. - Added a UserNotFoundException to test the function - Added a method for searching for a specific id like we had before - Added a put method with to if function to check if the user has entered name and password. - After the the update will start - Then I missed a return at first so I added that - Added comments

--- src/main/java/webservice_lektion6_uppgifter/Controller/UserController.java
+++ src/main/java/webservice_lektion6_uppgifter/Controller/UserController.java
@@ -1,8 +1,8 @@
 package webservice_lektion6_uppgifter.Controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import webservice_lektion6_uppgifter.Exceptions.UserNotFoundException;
 import webservice_lektion6_uppgifter.Model.User;
 import webservice_lektion6_uppgifter.Repository.UserRepository;
 
@@ -14,6 +14,13 @@ public class UserController {
     // Dependency Injektion av min repository
     @Autowired
     UserRepository userRepository;
+
+    //Metod för att hämta en användare baserat på ID
+    @GetMapping ("/{id}")
+    public ResponseEntity <User> getUserById(@PathVariable Long id){
+        return ResponseEntity.ok(userRepository.findById(id).orElseThrow(()-> new UserNotFoundException(id)));
+    }
+
 
     // Metod för att hämta alla användare
     @GetMapping
@@ -29,10 +36,29 @@ public class UserController {
         return ResponseEntity.ok(userRepository.save(user));
     }
 
-    @PutMapping
-    public ResponseEntity <User> updateUser(
-            @RequestBody User user
-    ){
-        return ResponseEntity.ok(userRepository.save(user));
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(
+            @PathVariable Long id,
+            @RequestBody User userInformation)
+    {
+        //Felhantering om något skulle saknas.
+        // Vi börjar med ID:t
+        if (userInformation.getName() == null) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+
+        if (userInformation.getPassword() == null) {
+            throw new IllegalArgumentException("Password cannot be empty");
+        }
+
+        User userToUpdate = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+
+        //? Uppdatera fälten som finns under användaren
+        userToUpdate.setName(userInformation.getName());
+        userToUpdate.setPassword(userInformation.getPassword());
+        userToUpdate.setEnabled(userInformation.getEnabled());
+
+        //! Behövs en return, för att spara och skicka all information
+        return ResponseEntity.ok(userRepository.save(userToUpdate));
     }
 }
